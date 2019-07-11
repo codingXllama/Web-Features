98d86f3bfe8d91eb32c2cfc54301599e01233775
Osama Al-Chalabi <osamaalchalabi2@gmail.com>
2019-07-11 13:55:55 -0600

    Created js file with a reset method

--- Full Screen Image Slider/img/app.js
+++ Full Screen Image Slider/img/app.js
@@ -1,1 +1,19 @@
-let sliderImages = document.querySelector
+// selecting all the elements that have class of slide
+
+let sliderImages = document.querySelectorAll(".slide");
+let arrowLeft = document.querySelector('#arrow-left'),
+    arrowRight = document.querySelector('#arrow-right');
+
+let currentImage = 0;
+
+
+
+
+// this function will reset function will reset everytime we visit an new image
+// it will set the display of all othe rimages to be none.
+function reset() {
+    // going through all image in slide
+    for (let i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = 'none';
+    }
+}
