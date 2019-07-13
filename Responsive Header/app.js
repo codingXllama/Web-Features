1563af75af02b261ad735d23c094f980ae094300
Osama Al-Chalabi <osamaalchalabi2@gmail.com>
2019-07-12 21:37:05 -0600

    Changed the background color and alpha for the scroll header

--- Responsive Header/app.js
+++ Responsive Header/app.js
@@ -4,7 +4,7 @@ $(window).on("scroll", function () {
     if ($(window).scrollTop()) {
 
         // $('nav').addClass('black');
-        $("header").css("background-color", "#333");
+        $("header").css("background-color", "rgba(87, 87, 87, 0.3)");
 
     } else {
         $("header").css("background-color", "transparent");
